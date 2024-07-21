14f71d153356a357c9409912bca2e4cb7b5a7d18
Plamen Petrov <biznetmen@gmail.com>
2024-07-21 13:15:09 +0300

    Turtle Graphics Package demo

---
+++ Day16/turtle.py
@@ -0,0 +1,13 @@
+from turtle import *
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("purple")
+timmy.forward(100)
+timmy.color("blue")
+timmy.left(80)
+timmy.color("red")
+timmy.left(380)
+my_screen = Screen()
+my_screen.exitonclick()
+
