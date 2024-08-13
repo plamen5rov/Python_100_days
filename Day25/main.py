0262d5cb417a9a4dab0a09ddb76fb2c9e4d31f8e
Plamen Petrov <biznetmen@gmail.com>
2024-08-13 12:27:00 +0300

    Reading CSV data with Pandas library

--- Day25/main.py
+++ Day25/main.py
@@ -1,16 +1,20 @@
 # with open("weather_data.csv") as data_file:
 #     data = data_file.readlines()
 
-import csv
+# import csv
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperature = []
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperature = []
     
-    for row in data:
-        if row[1] != 'temp':
-            temperature.append(int(row[1]))
-            print(row)
+#     for row in data:
+#         if row[1] != 'temp':
+#             temperature.append(int(row[1]))
+#             print(row)
             
     
-    print(temperature)    
+#     print(temperature)    
+
+import pandas as pd
+data = pd.read_csv("weather_data.csv")
+print(data)
