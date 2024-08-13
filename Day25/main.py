6dc167211f9d8a400146c6df225220335ab783d4
Plamen Petrov <biznetmen@gmail.com>
2024-08-13 11:54:01 +0300

    Getting Weather Data from a CVS file

---
+++ Day25/main.py
@@ -0,0 +1,16 @@
+# with open("weather_data.csv") as data_file:
+#     data = data_file.readlines()
+
+import csv
+
+with open("weather_data.csv") as data_file:
+    data = csv.reader(data_file)
+    temperature = []
+    
+    for row in data:
+        if row[1] != 'temp':
+            temperature.append(int(row[1]))
+            print(row)
+            
+    
+    print(temperature)    
