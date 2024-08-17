e052d7c472b1734625cb19778e2ea39080a606d2
Plamen Petrov <biznetmen@gmail.com>
2024-08-17 10:10:43 +0300

    Buttons, Entry, and Setting Component Options

--- Day27/sandbox.py
+++ Day27/sandbox.py
@@ -1,16 +1,28 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(800, 600)
 
 #Label
 
-my_label = tkinter.Label(text="I am a Label", font=("Courier New", 24, "bold"),)
+my_label = Label(text="I am a Label", font=("Courier New", 24, "bold"),)
 my_label.pack()
 
+#Buton
+
+def button_clicked():
+    my_label["text"]=input.get()
+    
+    
+my_button = Button(text="Click me!", font=("Courier New", 11, "normal"), command=button_clicked)
+my_button.pack()
 
 
+#Entry
+
+input = Entry(width=22)
+input.pack()
 
 
 
