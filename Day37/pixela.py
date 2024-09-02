dd7a0a941fc5ba6a3c09b6e0d0f7fb7fe9562cc7
Plamen Petrov <biznetmen@gmail.com>
2024-09-02 10:16:57 +0300

    Advanced Authentication using an HTTP Header

--- Day37/pixela.py
+++ Day37/pixela.py
@@ -1,15 +1,37 @@
 import requests
 
 pixela_endpoint = "https://pixe.la/v1/users"
+USER = "plam7"
+TOKEN = "fxq5guyvvbh"
 
 user_params = {
-    "token":"fxq5guyvvbh", 
-    "username":"plam7", 
+    "token":TOKEN, 
+    "username":USER, 
     "agreeTermsOfService":"yes", 
     "notMinor":"yes", 
     "thanksCode":"ThisIsThanksCode"
 }
 
-response = requests.post(url=pixela_endpoint, json=user_params, timeout=10)
+# response = requests.post(url=pixela_endpoint, json=user_params, timeout=10)
 
-print(response.status_code)
+graph_endpoint = f"{pixela_endpoint}/{USER}/graphs"
+
+graph_params = {
+    "id":"graph001",
+    "name":"coding-days",
+    "unit":"commit",
+    "type":"int",
+    "color":"sora",
+    "timezone":"Europe/Sofia",
+    "isSecret":False,
+    "publishOptionalData":True
+}
+
+headers = {"X-USER-TOKEN":TOKEN}
+
+response = requests.post(url=graph_endpoint, 
+                         json=graph_params, 
+                         headers=headers, 
+                         timeout=10)
+
+print(response.text)
