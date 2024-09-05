61828dfaf8c82db6730410708695845b3f22626e
Plamen Petrov <biznetmen@gmail.com>
2024-09-05 09:36:45 +0300

    Request a (bearer) Token from Amadeus

--- Day39/flight_search.py
+++ Day39/flight_search.py
@@ -1,17 +1,32 @@
 class FlightSearch:
 
     def __init__(self):
-        """
-        Initialize an instance of the FlightSearch class.
-        This constructor performs the following tasks:
-        1. Retrieves the API key and secret from the environment variables 'AMADEUS_API_KEY'
-        and 'AMADEUS_SECRET' respectively.
-        Instance Variables:
-        _api_key (str): The API key for authenticating with Amadeus, sourced from the .env file
-        _api_secret (str): The API secret for authenticating with Amadeus, sourced from the .env file.
-        _token (str): The authentication token obtained by calling the _get_new_token() method.
-        """
         self._api_key = os.environ["AMADEUS_API_KEY"]
         self._api_secret = os.environ["AMADEUS_SECRET"]
         # Getting a new token every time program is run. Could reuse unexpired tokens as an extension.
         self._token = self._get_new_token()
+
+    def _get_new_token(self):
+        """
+        Generates the authentication token used for accessing the Amadeus API and returns it.
+        This function makes a POST request to the Amadeus token endpoint with the required
+        credentials (API key and API secret) to obtain a new client credentials token.
+        Upon receiving a response, the function updates the FlightSearch instance's token.
+        Returns:
+            str: The new access token obtained from the API response.
+        """
+        # Header with content type as per Amadeus documentation
+        header = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        body = {
+            'grant_type': 'client_credentials',
+            'client_id': self._api_key,
+            'client_secret': self._api_secret
+        }
+        response = requests.post(url=TOKEN_ENDPOINT, headers=header, data=body)
+
+        # New bearer token. Typically expires in 1799 seconds (30min)
+        print(f"Your token is {response.json()['access_token']}")
+        print(f"Your token expires in {response.json()['expires_in']} seconds")
+        return response.json()['access_token']
