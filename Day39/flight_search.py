a80da873a63d94848c1d7a7ae85948e6994f870e
Plamen Petrov <biznetmen@gmail.com>
2024-09-05 09:39:17 +0300

    City Search - Make requests using the Amadeus Travel API

--- Day39/flight_search.py
+++ Day39/flight_search.py
@@ -1,3 +1,15 @@
+import requests
+from datetime import datetime
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+IATA_ENDPOINT = "https://test.api.amadeus.com/v1/reference-data/locations/cities"
+FLIGHT_ENDPOINT = "https://test.api.amadeus.com/v2/shopping/flight-offers"
+TOKEN_ENDPOINT = "https://test.api.amadeus.com/v1/security/oauth2/token"
+
 class FlightSearch:
 
     def __init__(self):
@@ -7,15 +19,6 @@ class FlightSearch:
         self._token = self._get_new_token()
 
     def _get_new_token(self):
-        """
-        Generates the authentication token used for accessing the Amadeus API and returns it.
-        This function makes a POST request to the Amadeus token endpoint with the required
-        credentials (API key and API secret) to obtain a new client credentials token.
-        Upon receiving a response, the function updates the FlightSearch instance's token.
-        Returns:
-            str: The new access token obtained from the API response.
-        """
-        # Header with content type as per Amadeus documentation
         header = {
             'Content-Type': 'application/x-www-form-urlencoded'
         }
@@ -25,8 +28,51 @@ class FlightSearch:
             'client_secret': self._api_secret
         }
         response = requests.post(url=TOKEN_ENDPOINT, headers=header, data=body)
-
         # New bearer token. Typically expires in 1799 seconds (30min)
         print(f"Your token is {response.json()['access_token']}")
         print(f"Your token expires in {response.json()['expires_in']} seconds")
         return response.json()['access_token']
+
+    def get_destination_code(self, city_name):
+        """
+        Retrieves the IATA code for a specified city using the Amadeus Location API.
+        Parameters:
+        city_name (str): The name of the city for which to find the IATA code.
+        Returns:
+        str: The IATA code of the first matching city if found; "N/A" if no match is found due to an IndexError, 
+        or "Not Found" if no match is found due to a KeyError.
+        
+        The function sends a GET request to the IATA_ENDPOINT with a query that specifies the city
+        name and other parameters to refine the search. It then attempts to extract the IATA code
+        from the JSON response. 
+        - If the city is not found in the response data (i.e., the data array is empty, leading to 
+        an IndexError), it logs a message indicating that no airport code was found for the city and 
+        returns "N/A".
+        - If the expected key is not found in the response (i.e., the 'iataCode' key is missing, leading 
+        to a KeyError), it logs a message indicating that no airport code was found for the city 
+        and returns "Not Found".
+        """
+        print(f"Using this token to get destination {self._token}")
+        headers = {"Authorization": f"Bearer {self._token}"}
+        query = {
+            "keyword": city_name,
+            "max": "2",
+            "include": "AIRPORTS",
+        }
+        response = requests.get(
+            url=IATA_ENDPOINT,
+            headers=headers,
+            params=query
+        )
+        
+        print(f"Status code {response.status_code}. Airport IATA: {response.text}")
+        try:
+            code = response.json()["data"][0]['iataCode']
+        except IndexError:
+            print(f"IndexError: No airport code found for {city_name}.")
+            return "N/A"
+        except KeyError:
+            print(f"KeyError: No airport code found for {city_name}.")
+            return "Not Found"
+
+        return code
