3294efb40d148e58e0d195620b1bffb6d3bd27c0
Plamen Petrov <biznetmen@gmail.com>
2024-09-06 10:09:52 +0300

    Step 4 - Retrieve your customer emails

--- Day40/data_manager.py
+++ Day40/data_manager.py
@@ -1,4 +1,5 @@
 import os
+from pprint import pprint
 import requests
 from requests.auth import HTTPBasicAuth
 from dotenv import load_dotenv
@@ -6,24 +7,27 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-SHEETY_PRICES_ENDPOINT = YOUR ENDPOINT HERE
-
-
 class DataManager:
 
     def __init__(self):
-        self._user = os.environ["SHEETY_USRERNAME"]
+
+        self._user = os.environ["SHEETY_USERNAME"]
         self._password = os.environ["SHEETY_PASSWORD"]
+        # Save your Sheety endpoints an environment variables
+        self.prices_endpoint = os.environ["SHEETY_PRICES_ENDPOINT"]
+        self.users_endpoint = os.environ["SHEETY_USERS_ENDPOINT"]
         self._authorization = HTTPBasicAuth(self._user, self._password)
+        # Destination and Customer fields data start out empty
         self.destination_data = {}
+        self.customer_data = {}
 
     def get_destination_data(self):
         # Use the Sheety API to GET all the data in that sheet and print it out.
-        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        response = requests.get(url=self.prices_endpoint)
         data = response.json()
-        self.destination_data = data["prices"]
         # Try importing pretty print and printing the data out again using pprint() to see it formatted.
         # pprint(data)
+        self.destination_data = data["prices"]
         return self.destination_data
 
     # In the DataManager Class make a PUT request and use the row id from sheet_data
@@ -36,7 +40,19 @@ class DataManager:
                 }
             }
             response = requests.put(
-                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                url=f"{self.prices_endpoint}/{city['id']}",
                 json=new_data
             )
             print(response.text)
+
+            
+            
+    def get_customer_emails(self):
+        response = requests.get(url=self.users_endpoint)
+        data = response.json()
+        # See how Sheet data is formatted so that you use the correct column name!
+        # pprint(data)
+        # Name of spreadsheet 'tab' with the customer emails should be "users".
+        self.customer_data = data["users"]
+        return self.customer_data
+
--- Day40/main.py
+++ Day40/main.py
@@ -1,37 +1,18 @@
 # more code above
 
+data_manager.destination_data = sheet_data
+data_manager.update_destination_codes()
+
+# ==================== Retrieve your customer emails ====================
+
+customer_data = data_manager.get_customer_emails()
+# Verify the name of your email column in your sheet. Yours may be different from mine
+customer_email_list = [row["whatIsYourEmail?"] for row in customer_data]
+# print(f"Your email list includes {customer_email_list}")
+
 # ==================== Search for direct flights  ====================
 
 tomorrow = datetime.now() + timedelta(days=1)
 six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
 
-for destination in sheet_data:
-    print(f"Getting direct flights for {destination['city']}...")
-    flights = flight_search.check_flights(
-        ORIGIN_CITY_IATA,
-        destination["iataCode"],
-        from_time=tomorrow,
-        to_time=six_month_from_today
-    )
-    cheapest_flight = find_cheapest_flight(flights)
-    print(f"{destination['city']}: £{cheapest_flight.price}")
-    # Slowing down requests to avoid rate limit
-    time.sleep(2)
-
-    # ==================== Search for indirect flight if N/A ====================
-
-    if cheapest_flight.price == "N/A":
-        print(f"No direct flight to {destination['city']}. Looking for indirect flights...")
-        stopover_flights = flight_search.check_flights(
-            ORIGIN_CITY_IATA,
-            destination["iataCode"],
-            from_time=tomorrow,
-            to_time=six_month_from_today,
-            is_direct=False
-        )
-        cheapest_flight = find_cheapest_flight(stopover_flights)
-        print(f"Cheapest indirect flight price is: £{cheapest_flight.price}")
-
-    # ==================== Send Notifications and Emails  ====================
-
-    # more code below
+# more code below
