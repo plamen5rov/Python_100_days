26daaba93aab7f1cc624084c040b9df94bd29784
Plamen Petrov <biznetmen@gmail.com>
2024-09-08 18:49:59 +0300

    Parsing HTML and Making Soup

---
+++ Day45/main.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+
+with open('website.html') as file:
+    content = file.read()
+
+soup = BeautifulSoup(content, "html.parser")
+
+# print(soup.title.string)
+
+print(soup.prettify())
