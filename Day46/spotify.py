34164559cf2c2581c2fa82ee2990f80a3d895f71
Plamen Petrov <biznetmen@gmail.com>
2024-09-10 11:18:54 +0300

    Step 2 - Authentication with Spotify

---
+++ Day46/spotify.py
@@ -0,0 +1,15 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id=YOUR UNIQUE CLIENT ID,
+        client_secret=YOUR UNIQUE CLIENT SECRET,
+        show_dialog=True,
+        cache_path="token.txt",
+        username=YOUR SPOTIFY DISPLAY NAME, 
+    )
+)
+user_id = sp.current_user()["id"]
