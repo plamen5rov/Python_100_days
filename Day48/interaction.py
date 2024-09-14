e14fcd84b2c5539e2a72ae8d3b496d1c03a3b534
Plamen Petrov <biznetmen@gmail.com>
2024-09-14 13:09:21 +0300

    Challenge: Use Selenium in a Blank Project & Scrape a Different Piece of Data

---
+++ Day48/interaction.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+# Deprecated - no longer needed
+chrome_driver_path = "/Users/philippmuellauer/Development/chromedriver"
+
+# keeps chrome open
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+# driver = webdriver.Chrome(executable_path=chrome_driver_path)
+# driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get("https://en.wikipedia.org/wiki/Main_Page")
+
+articles_number = driver.find_element(by=By.XPATH, value='//*[@id="articlecount"]/a[1]')
+
+print(articles_number.text)
