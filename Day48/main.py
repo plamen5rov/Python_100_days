12d80258f5a0e7f06bd5feae064d343fb551b97b
Plamen Petrov <biznetmen@gmail.com>
2024-09-14 12:15:25 +0300

    Install & Set Up Selenium

--- Day48/main.py
+++ Day48/main.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome(options=chrome_options)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
 driver.get('https://planethereum.com')
 
-driver.close() #closes one tab
-driver.quit() #closes the entire browser
+driver.close()  # closes one tab
+driver.quit()   # closes the entire browser
