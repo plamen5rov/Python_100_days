84f69a697e6a1b041cc91d69e1ef1a97ae51aa51
Plamen Petrov <biznetmen@gmail.com>
2024-09-15 10:55:34 +0300

    Step 4 - Apply for all the jobs

--- Day49/main.py
+++ Day49/main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 import time
 
@@ -7,18 +8,26 @@ ACCOUNT_EMAIL = YOUR LOGIN EMAIL
 ACCOUNT_PASSWORD = YOUR LOGIN PASSWORD
 PHONE = YOUR PHONE NUMBER
 
+
+def abort_application():
+    # Click Close Button
+    close_button = driver.find_element(by=By.CLASS_NAME, value="artdeco-modal__dismiss")
+    close_button.click()
+
+    time.sleep(2)
+    # Click Discard Button
+    discard_button = driver.find_elements(by=By.CLASS_NAME, value="artdeco-modal__confirm-dialog-btn")[1]
+    discard_button.click()
+
+
 # Optional - Keep the browser open if the script crashes.
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
 
-driver.get(
-    "https://www.linkedin.com/jobs/search/?f_LF=f_AL&geoId=102257491"
-    "&keywords=python%20developer"
-    "&location=London%2C%20England%2C%20United%20Kingdom"
-    "&redirect=false&position=1&pageNum=0"
-)
+driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3586148395&f_LF=f_AL&geoId=101356765&"
+           "keywords=python&location=London%2C%20England%2C%20United%20Kingdom&refresh=true")
 
 # Click Reject Cookies Button
 time.sleep(2)
@@ -38,20 +47,50 @@ password_field = driver.find_element(by=By.ID, value="password")
 password_field.send_keys(ACCOUNT_PASSWORD)
 password_field.send_keys(Keys.ENTER)
 
-# You may be presented with a CAPTCHA - Solve the Puzzle Manually
+# CAPTCHA - Solve Puzzle Manually
 input("Press Enter when you have solved the Captcha")
 
-#Locate the apply button
+# Get Listings
 time.sleep(5)
-apply_button = driver.find_element(by=By.CSS_SELECTOR, value=".jobs-s-apply button")
-apply_button.click()
+all_listings = driver.find_elements(by=By.CSS_SELECTOR, value=".job-card-container--clickable")
 
-#If application requires phone number and the field is empty, then fill in the number.
+# Apply for Jobs
+for listing in all_listings:
+    print("Opening Listing")
+    listing.click()
+    time.sleep(2)
+    try:
+        # Click Apply Button
+        apply_button = driver.find_element(by=By.CSS_SELECTOR, value=".jobs-s-apply button")
+        apply_button.click()
+
+        # Insert Phone Number
+        # Find an <input> element where the id contains phoneNumber
+        time.sleep(5)
+        phone = driver.find_element(by=By.CSS_SELECTOR, value="input[id*=phoneNumber]")
+        if phone.text == "":
+            phone.send_keys(PHONE)
+
+        # Check the Submit Button
+        submit_button = driver.find_element(by=By.CSS_SELECTOR, value="footer button")
+        if submit_button.get_attribute("data-control-name") == "continue_unify":
+            abort_application()
+            print("Complex application, skipped.")
+            continue
+        else:
+            # Click Submit Button
+            print("Submitting job application")
+            submit_button.click()
+
+        time.sleep(2)
+        # Click Close Button
+        close_button = driver.find_element(by=By.CLASS_NAME, value="artdeco-modal__dismiss")
+        close_button.click()
+
+    except NoSuchElementException:
+        abort_application()
+        print("No application button, skipped.")
+        continue
+
 time.sleep(5)
-phone = driver.find_element(by=By.CSS_SELECTOR, value="input[id*=phoneNumber]")
-if phone.text == "":
-    phone.send_keys(PHONE)
-
-#Submit the application
-submit_button = driver.find_element(by=By.CSS_SELECTOR, value="footer button")
-submit_button.click()
+driver.quit()
