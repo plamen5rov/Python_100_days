e7978bd89861108aa4214343c0325c1edbe731a8
Plamen Petrov <biznetmen@gmail.com>
2024-09-21 12:35:19 +0300

    Instagram Followers Bot -  Step 4 - Find the followers of the target account

--- Day52/main.py
+++ Day52/main.py
@@ -19,30 +19,15 @@ PASSWORD = os.getenv('PASSWORD')
 
 
 
-# class InstaFollower:
+class InstaFollower:
 
-#     def __init__(self):
-#         # Optional - Keep browser open (helps diagnose issues during a crash)
-#         chrome_options = webdriver.ChromeOptions()
-#         chrome_options.add_experimental_option("detach", True)
-#         self.driver = webdriver.Chrome(options=chrome_options)
+    def __init__(self):
+        # Optional - Keep browser open (helps diagnose issues during a crash)
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_experimental_option("detach", True)
+        self.driver = webdriver.Chrome(options=chrome_options)
 
-#     def login(self):
-#         pass
-
-#     def find_followers(self):
-#         pass
-
-#     def follow(self):
-#         pass
-
-
-# bot = InstaFollower()
-# bot.login()
-# bot.find_followers()
-# bot.follow()
-
-def login(self):
+    def login(self):
         url = "https://www.instagram.com/accounts/login/"
         self.driver.get(url)
         time.sleep(4.2)
@@ -74,3 +59,30 @@ def login(self):
         notifications_prompt = self.driver.find_element(by=By.XPATH, value="// button[contains(text(), 'Not Now')]")
         if notifications_prompt:
             notifications_prompt.click()
+
+    def find_followers(self):
+        time.sleep(5)
+        # Show followers of the selected account. 
+        self.driver.get(f"https://www.instagram.com/{SIMILAR_ACCOUNT}/followers")
+
+        time.sleep(5.2)
+        # The xpath of the modal that shows the followers will change over time. Update yours accordingly.
+        modal_xpath = "/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]"
+        modal = self.driver.find_element(by=By.XPATH, value=modal_xpath)
+        for i in range(10):
+            # In this case we're executing some Javascript, that's what the execute_script() method does.
+            # The method can accept the script as well as an HTML element.
+            # The modal in this case, becomes the arguments[0] in the script.
+            # Then we're using Javascript to say: "scroll the top of the modal (popup) element by the height of the modal (popup)"
+            self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", modal)
+            time.sleep(2)
+
+    def follow(self):
+        pass
+
+
+bot = InstaFollower()
+bot.login()
+bot.find_followers()
+bot.follow()
+
