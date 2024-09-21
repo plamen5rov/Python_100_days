969b26b57490d92a91ccebef6f6c6608262efc8f
Plamen Petrov <biznetmen@gmail.com>
2024-09-21 12:31:25 +0300

    Instagram Followers Bot -

--- Day52/main.py
+++ Day52/main.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from dotenv import load_dotenv
 import os
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 
 # Load the .env file
 load_dotenv()
@@ -16,25 +19,58 @@ PASSWORD = os.getenv('PASSWORD')
 
 
 
-class InstaFollower:
+# class InstaFollower:
 
-    def __init__(self):
-        # Optional - Keep browser open (helps diagnose issues during a crash)
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_experimental_option("detach", True)
-        self.driver = webdriver.Chrome(options=chrome_options)
+#     def __init__(self):
+#         # Optional - Keep browser open (helps diagnose issues during a crash)
+#         chrome_options = webdriver.ChromeOptions()
+#         chrome_options.add_experimental_option("detach", True)
+#         self.driver = webdriver.Chrome(options=chrome_options)
 
-    def login(self):
-        pass
+#     def login(self):
+#         pass
 
-    def find_followers(self):
-        pass
+#     def find_followers(self):
+#         pass
 
-    def follow(self):
-        pass
+#     def follow(self):
+#         pass
 
 
-bot = InstaFollower()
-bot.login()
-bot.find_followers()
-bot.follow()
+# bot = InstaFollower()
+# bot.login()
+# bot.find_followers()
+# bot.follow()
+
+def login(self):
+        url = "https://www.instagram.com/accounts/login/"
+        self.driver.get(url)
+        time.sleep(4.2)
+
+        # Check if the cookie warning is present on the page
+        decline_cookies_xpath = "/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div[2]/div/button[2]"
+        cookie_warning = self.driver.find_elements(By.XPATH, decline_cookies_xpath)
+        if cookie_warning:
+            # Dismiss the cookie warning by clicking an element or button
+            cookie_warning[0].click()
+
+        username = self.driver.find_element(by=By.NAME, value="username")
+        password = self.driver.find_element(by=By.NAME, value="password")
+
+        username.send_keys(USERNAME)
+        password.send_keys(PASSWORD)
+
+        time.sleep(2.1)
+        password.send_keys(Keys.ENTER)
+
+        time.sleep(4.3)
+        # Click "Not now" and ignore Save-login info prompt
+        save_login_prompt = self.driver.find_element(by=By.XPATH, value="//div[contains(text(), 'Not now')]")
+        if save_login_prompt:
+            save_login_prompt.click()
+
+        time.sleep(3.7)
+        # Click "not now" on notifications prompt
+        notifications_prompt = self.driver.find_element(by=By.XPATH, value="// button[contains(text(), 'Not Now')]")
+        if notifications_prompt:
+            notifications_prompt.click()
