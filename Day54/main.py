301893ceac56585ef8d440aae35db21b019a2193
Plamen Petrov <biznetmen@gmail.com>
2024-10-05 14:05:33 +0300

    Understanding Python Decorator Functions and the @ Syntax

--- Day54/main.py
+++ Day54/main.py
@@ -2,9 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/')
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return 'Hello, World!'
+
+@app.route("/bye")
+def say_bye():
+    return "Bye"
 
 if __name__ == "__main__":
     app.run()
