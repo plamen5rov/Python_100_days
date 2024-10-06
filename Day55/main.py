5cd5d6997a8bf6acf2c3af049ff8c0c4011ad82a
Plamen Petrov <biznetmen@gmail.com>
2024-10-06 17:35:39 +0300

    Use Python Decorators to Style HTML Tags

--- Day55/main.py
+++ Day55/main.py
@@ -2,8 +2,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def make_bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+    return wrapper
+
+def make_emphasis(function):
+    def wrapper():
+        return "<em>" + function() + "</em>"
+    return wrapper
+
+def make_underlined(function):
+    def wrapper():
+        return "<u>" + function() + "</u>"
+    return wrapper
 
 @app.route('/')
+@make_bold
+@make_emphasis
+@make_underlined
 def hello_world():
     return '<h2 style="color: green">Hello, World!</h2>'
         
