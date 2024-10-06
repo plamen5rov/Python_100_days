756133398a0757f2158a3ab52345133bc596eac8
Plamen Petrov <biznetmen@gmail.com>
2024-10-06 17:30:21 +0300

    Rendering HTML Elements with Flask

--- Day55/main.py
+++ Day55/main.py
@@ -5,15 +5,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return '<h2 style="color: green">Hello, World!</h2>'
+        
+
 
 @app.route("/bye")
 def say_bye():
-    return "Bye"
+    return '<img src="https://media.giphy.com/media/BcuDuUQgeSQco/giphy.gif?cid=ecf05e47snr8ispqi7im5g1akuqwa2pov6pzx221fiemez52&ep=v1_gifs_related&rid=giphy.gif&ct=g">'
 
 @app.route("/<username>")
 def greet(username):
-    return f"Hallo, {username}"
+    return f"Hallo,{username}"
 
 
 if __name__ == "__main__":
