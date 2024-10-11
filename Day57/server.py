16182d5597b014e7aa5264a8046f05e24546397f
Plamen Petrov <biznetmen@gmail.com>
2024-10-11 15:39:06 +0300

    Multiline Statements with Jinja

--- Day57/server.py
+++ Day57/server.py
@@ -26,5 +26,12 @@ def guess(name):
     
     return render_template('guess.html', name=name, gender=gender, age=age)
 
+@app.route('/blog')
+def blog():
+    blog_url = 'https://api.npoint.io/c790b4d5cab58020d391'
+    blog_data = requests.get(blog_url).json()
+    return render_template('blog.html', posts=blog_data)
+    
+
 if __name__ == '__main__':
     app.run(debug=True)
