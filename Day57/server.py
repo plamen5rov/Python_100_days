d4055535e6cb08cc5847c7f4814f271cb9f032a7
Plamen Petrov <biznetmen@gmail.com>
2024-10-11 13:35:46 +0300

    Combining Jinja Templating with APIs

--- Day57/server.py
+++ Day57/server.py
@@ -1,5 +1,5 @@
 """Using Jinja to Produce Dynamic HTML Pages"""
-
+import requests
 import random
 import datetime
 from flask import Flask, render_template
@@ -12,6 +12,19 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html', num=random_number, year = x.year)
+
+@app.route('/guess/<name>')
+def guess(name):
+    gender_url = f"https://api.genderize.io?name={name}"
+    gender_response = requests.get(gender_url)
+    gender_data = gender_response.json()
+    gender = gender_data["gender"]
+    age_url = f"https://api.agify.io?name={name}"
+    age_response = requests.get(age_url)
+    age_data = age_response.json()
+    age = age_data["age"]
+    
+    return render_template('guess.html', name=name, gender=gender, age=age)
 
 if __name__ == '__main__':
     app.run(debug=True)
