3e579550b6e07c6aa2de6d297129c6ddb62301d6
Plamen Petrov <biznetmen@gmail.com>
2024-10-17 16:01:42 +0300

    Rendering Individual Post

--- Day59/main.py
+++ Day59/main.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 import requests
 
-#USE YOUR OWN npoint LINK! OTHERWISE IT WILL NOT WORK 👇
-posts = requests.get("https://api.npoint.io/383a75fc415a9b5d35f3").json()
+# USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
 
 app = Flask(__name__)
 
@@ -22,5 +22,14 @@ def contact():
     return render_template("contact.html")
 
 
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
