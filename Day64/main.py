f9cb91d477078a97c5b72435da7e0420aa037f8e
Plamen Petrov <biznetmen@gmail.com>
2024-11-13 09:11:25 +0200

    Day 64 L487 - Edit a movie rating and review (SQL Alchemy 3.1)

--- Day64/main.py
+++ Day64/main.py
@@ -34,38 +34,29 @@ class Movie(db.Model):
 with app.app_context():
     db.create_all()
 
-## After adding the new_movie the code needs to be commented out/deleted.
-## So you are not trying to add the same movie twice.
-# new_movie = Movie(
-#     title="Phone Booth",
-#     year=2002,
-#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
-#     rating=7.3,
-#     ranking=10,
-#     review="My favourite character was the caller.",
-#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
-# )
-# second_movie = Movie(
-#     title="Avatar The Way of Water",
-#     year=2022,
-#     description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
-#     rating=7.3,
-#     ranking=9,
-#     review="I liked the water.",
-#     img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
-# )
-# with app.app_context():
-#     db.session.add(new_movie)
-#     db.session.add(second_movie)
-#     db.session.commit()
-
+class RateMovieForm(FlaskForm):
+    rating = StringField("Your Rating Out of 10 e.g. 7.5")
+    review = StringField("Your Review")
+    submit = SubmitField("Done")
 
 @app.route("/")
 def home():
     result = db.session.execute(db.select(Movie))
-    all_movies = result.scalars().all()
+    all_movies = result.scalars()
     return render_template("index.html", movies=all_movies)
 
+# Adding the Update functionality
+@app.route("/edit", methods=["GET", "POST"])
+def rate_movie():
+    form = RateMovieForm()
+    movie_id = request.args.get("id")
+    movie = db.get_or_404(Movie, movie_id)
+    if form.validate_on_submit():
+        movie.rating = float(form.rating.data)
+        movie.review = form.review.data
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("edit.html", movie=movie, form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
